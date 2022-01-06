5361a66af974aca2bed1a35ff0dffe145e23f5d0
Dor Mitnik <95883449+DorMitnik@users.noreply.github.com>
2022-01-06 18:33:11 +0200

    Update client.py

--- client.py
+++ client.py
@@ -5,32 +5,36 @@ import threading
 import pyautogui as pg
 import pickle as pickle
 
-
 pg.PAUSE = 0  # Pause between the moveTo function
 server_IP = input("Enter the server IP address: ")
 
 
 class Client:
     def __init__(self):
-        """Initialize the connections with the server"""
-        self.client = ScreenShareClient(server_IP, 5050, x_res=1920, y_res=1080)
-        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.running = True
+        self.streaming = False
 
     def streaming_function(self):
-        """Shares the screen with the server"""
-        print("[START STREAMING]....")
-        self.client.start_stream()
-        while self.running:
-            time.sleep(1)
+        """Starts/stops the streaming and the mouse tracking"""
+        if self.streaming:
+            print("[START STREAMING]....")
+            self.client = ScreenShareClient(server_IP, 5050, x_res=1920, y_res=1080)
+            self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conn.connect((server_IP, 1234))
+            self.client.start_stream()
+            self.receive_message()
+        else:
+            print("[STOP STREAMING]....")
+            self.client.stop_stream()
+            self.conn.close()
 
-        print("[STOP STREAMING]....")
-        self.client.stop_stream()
+    def config_system(self):
+        """When clicked the flag is changed to start/stop streaming"""
+        self.streaming = not self.streaming
+        self.streaming_function()
 
     def receive_message(self):
         """Receive message and moves/click"""
-        self.conn.connect((server_IP, 1234))
-        while True:
+        while self.streaming:
             data = pickle.loads(self.conn.recv(4096))
             if isinstance(data, pg.Point):
                 self.move_mouse_location(data)
@@ -58,12 +62,7 @@ class Client:
 
 
 def main():
-    client = Client()
-    t = threading.Thread(target=client.streaming_function)
-    t2 = threading.Thread(target=client.receive_message)
-
-    t.start()
-    t2.start()
+    pass
 
 
 main()
