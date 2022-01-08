1f7bcb6933dbe99e2d61a3e76663c8bb21086228
Dor Mitnik <95883449+DorMitnik@users.noreply.github.com>
2022-01-08 13:52:58 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,37 +2,49 @@ from vidstream import ScreenShareClient
 import socket
 import pyautogui as pg
 import pickle as pickle
+import threading
 
 pg.PAUSE = 0  # Pause between the moveTo function
-server_IP = input("Enter the server IP address: ")
 
 
 class Client:
     def __init__(self):
-        self.streaming = False
+        self.sharing = False
 
-    def streaming_function(self):
-        """Starts/stops the streaming and the mouse tracking"""
-        if self.streaming:
+    def streaming_function(self, server_IP):
+        """Takes the IP and starting the streaming and the mouse tracking"""
+        if self.sharing:
             print("[START STREAMING]....")
             self.client = ScreenShareClient(server_IP, 5050, x_res=1920, y_res=1080)
             self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.conn.connect((server_IP, 1234))
             self.client.start_stream()
-            self.receive_message()
+            t = threading.Thread(target=self.receive_message)
+            t.start()
         else:
-            print("[STOP STREAMING]....")
             self.client.stop_stream()
             self.conn.close()
+            print("\n[CONNECTION CLOSED]....")
 
-    def config_system(self):
-        """When clicked the flag is changed to start/stop streaming"""
-        self.streaming = not self.streaming
-        self.streaming_function()
+    def start_server(self, server_IP):
+        """Takes the Server IP and enabling the server"""
+        if not self.sharing:
+            self.sharing = True
+            self.streaming_function(server_IP)
+        else:
+            pass
+
+    def stop_server(self, server_IP):
+        """Takes the Server IP and stops the server"""
+        if self.sharing:
+            self.sharing = False
+            self.streaming_function(server_IP)
+        else:
+            pass
 
     def receive_message(self):
         """Receive message and moves/click"""
-        while self.streaming:
+        while self.sharing:
             data = pickle.loads(self.conn.recv(4096))
             if isinstance(data, pg.Point):
                 self.move_mouse_location(data)
@@ -47,20 +59,13 @@ class Client:
         try:
             pg.moveTo(location.x, location.y, )
 
-        except KeyboardInterrupt:
-            print("There's an error..")
+        except KeyboardInterrupt as e:
+            raise Exception(e)
 
     @staticmethod
     def mouse_click():
         """Clicking the mouse"""
         try:
             pg.click()
-        except KeyboardInterrupt:
-            print("There's an error..")
-
-
-def main():
-    pass
-
-
-main()
+        except KeyboardInterrupt as e:
+            raise Exception(e)
